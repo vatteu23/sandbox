340fd80e43c183558987594ce09e5126fd9f1b27
Uday Vatti <uvatti@labelbox.com>
2024-02-19 17:33:09 -0600

    updates

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,12 @@ const nextConfig = {
   reactStrictMode: false,
   typescript: {
     ignoreBuildErrors: true,
-  }
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
