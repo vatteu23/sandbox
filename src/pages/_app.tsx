4a8220661407658dbac3dcbf719786d793f78c93
Uday Vatti <uvatti@labelbox.com>
2024-02-20 11:40:16 -0600

    added gtm

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,14 @@ export default function App({ Component, pageProps }: AppProps) {
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
     })(window, document, "clarity", "script", "l4tnkfxbwl");
+
+    <!-- Google Tag Manager -->
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NVGZ798K');
+<!-- End Google Tag Manager -->
       `}
     </Script>
   <Component {...pageProps} /></>;
